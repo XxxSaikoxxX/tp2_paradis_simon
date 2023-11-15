280741625876742d5cfc8006b49cfd46afedbf82
XxxSaikoxxX <sparadis35@gmail.com>
2023-11-15 09:33:56 -0500

    index

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-test
+<form action="address_form.php" method="POST">
+    Combien d'adresses avez-vous ? <input type="number" name="nb_addresses" min="1">
+    <input type="submit" value="Envoyer">
+</form>
