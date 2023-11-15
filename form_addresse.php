e904d304059456de07d6de9ae9db942b3a3f6a01
XxxSaikoxxX <sparadis35@gmail.com>
2023-11-15 09:37:15 -0500

    formulaire addresse

---
+++ form_addresse.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nb_addresses = $_POST['nb_addresses'];
+
+    echo '<form action="save_addresses.php" method="POST">';
+    for ($i = 0; $i < $nb_addresses; $i++) {
+        echo 'Adresse ' . ($i + 1) . ':<br>';
+        echo 'Rue: <input type="text" name="street[]" maxlength="50"><br>';
+        echo 'Numéro: <input type="number" name="street_nb[]"><br>';
+        echo 'Type: <select name="type[]"><option value="livraison">Livraison</option><option value="facturation">Facturation</option><option value="autre">Autre</option></select><br>';
+        echo 'Ville: <select name="city[]"><option value="Montréal">Montréal</option><option value="Laval">Laval</option></select><br>';
+        echo 'Code postal: <input type="text" name="zipcode[]" maxlength="6"><br><br>';
+    }
+    echo '<input type="submit" value="Envoyer">';
+    echo '</form>';
+}
+?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<form action="address_form.php" method="POST">
+<form action="form_addresse.php" method="POST">
     Combien d'adresses avez-vous ? <input type="number" name="nb_addresses" min="1">
     <input type="submit" value="Envoyer">
 </form>
